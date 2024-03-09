2a08b000a29b693519f78eda01c1a59b7aea3dc2
AdaiasMartins <110681069+AdaiasMartins@users.noreply.github.com>
2024-03-09 15:17:13 -0300

    Initial commit

---
+++ src/main/java/com/example/PlataformaDeAssinaturas/PlataformaDeAssinaturasApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlataformaDeAssinaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDeAssinaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDeAssinaturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PlataformaDeAssinaturas/PlataformaDeAssinaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlataformaDeAssinaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDeAssinaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
